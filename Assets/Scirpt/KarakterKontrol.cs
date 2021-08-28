59b94b5ad9a5b2c7152251e4dd47a68f88ab0bd8
Talha Şeref Alataş <talhaalatas97@gmail.com>
2021-08-28 21:44:24 +0300

    Initial commit

---
+++ Assets/Scirpt/KarakterKontrol.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KarakterKontrol : MonoBehaviour
+{
+    SpriteRenderer animasyonSprite;
+    public Sprite[] animasyonDizi;
+    Rigidbody2D fizik;
+    int animasyonSayac = 0;
+    bool animasyonKontrol = true;
+    float animasyonZamani = 0;
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody2D>();
+        animasyonSprite = GetComponent<SpriteRenderer>();
+        
+    }
+    void Update()
+    {
+        Animasyon();
+        Hareket();
+    }
+
+    void Animasyon()
+    {
+        if (animasyonKontrol)
+        {
+            animasyonSprite.sprite = animasyonDizi[animasyonSayac];
+            animasyonSayac++;
+            if (animasyonSayac == animasyonDizi.Length)
+            {
+                animasyonSayac--;
+                animasyonKontrol = false;
+            }
+        }
+        else
+        {
+            animasyonSprite.sprite = animasyonDizi[animasyonSayac];
+            animasyonSayac--;
+            if (animasyonSayac == 0)
+            {
+                animasyonKontrol = true;
+            }
+        }
+    }
+    void Hareket()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            fizik.velocity = new Vector2(0, 0);
+            fizik.AddForce(new Vector2(0, 180));
+        }
+        if(fizik.velocity.y>0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 45);
+        }
+        if(fizik.velocity.y<0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, -45);
+        }
+    }
+}
---
+++ Assets/Scirpt/OyunKontrol.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyunKontrol : MonoBehaviour
+{
+    public GameObject gökyüzüBir;
+    public GameObject gökyüzüIki;
+    public GameObject engel;
+    GameObject[] engelDizisi;
+    Rigidbody2D fizikBir;
+    Rigidbody2D fizikIki;
+    float arkaPlanHız = -1.8f;
+    float arkaPlanUzunluk = 0;
+    float engelOlusturmaZaman = 0;
+    int engelSayac = 0;
+
+    void Start()
+    {
+        fizikBir = gökyüzüBir.GetComponent<Rigidbody2D>();
+        fizikIki = gökyüzüIki.GetComponent<Rigidbody2D>();
+
+        fizikBir.velocity = new Vector2(arkaPlanHız, 0);
+        fizikIki.velocity = new Vector2(arkaPlanHız, 0);
+        arkaPlanUzunluk = gökyüzüBir.GetComponent<BoxCollider2D>().size.x;
+
+        for (int i = 0; i < engelDizisi.Length; i++)
+        {
+            engelDizisi[i] = Instantiate(engel, new Vector2(2.71f, 0), Quaternion.identity);
+            Rigidbody2D engelFiziği = engelDizisi[i].AddComponent<Rigidbody2D>();
+            engelFiziği.gravityScale = 0;
+            engelFiziği.velocity = new Vector2(arkaPlanHız, 0);
+
+
+        }
+    }
+
+
+    void Update()
+    {
+        if(gökyüzüBir.transform.position.x <= -arkaPlanUzunluk)
+        {
+            gökyüzüBir.transform.position += new Vector3(2 * arkaPlanUzunluk, 0);
+        }
+        if(gökyüzüIki.transform.position.x <= -arkaPlanUzunluk)
+        {
+            gökyüzüIki.transform.position += new Vector3(2 * arkaPlanUzunluk,0);
+        }
+
+        engelOlusturmaZaman = Time.deltaTime;
+        if(engelOlusturmaZaman>=2f)
+        {
+            engelOlusturmaZaman = 0;
+            float yEksen = Random.Range(0.17f, 2.18f);
+            engelDizisi[engelSayac].transform.position = new Vector3(3, yEksen);
+            engelSayac++;
+            if(engelSayac==engelDizisi.Length)
+            {
+                engelSayac = 0;
+
+            }
+        }
+
+        
+    }
+}
